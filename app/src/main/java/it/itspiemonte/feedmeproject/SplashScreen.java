8ee597d06433905d2d7e7c35ebb08e04b71cbde9
pedrolis <stefano.pedroli@edu.itspiemonte.it>
2017-06-07 11:10:54 +0200

    Add splah activity

--- app/src/main/java/it/itspiemonte/feedmeproject/SplashScreen.java
+++ app/src/main/java/it/itspiemonte/feedmeproject/SplashScreen.java
@@ -1,13 +1,37 @@
 package it.itspiemonte.feedmeproject;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
     }
 }
