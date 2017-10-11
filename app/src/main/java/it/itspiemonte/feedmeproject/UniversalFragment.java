f5efe9a130312fb3d0186054c7b6d88885147291
Keterbano <simone.marca@edu.itspiemonte.it>
2017-10-11 10:02:01 +0200

    Tutoria con i fragment (v0.5)

--- app/src/main/java/it/itspiemonte/feedmeproject/UniversalFragment.java
+++ app/src/main/java/it/itspiemonte/feedmeproject/UniversalFragment.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import static android.support.v4.content.res.ResourcesCompat.getDrawable;
+
 /**
  * Created by Simone on 20/06/2017.
  */
@@ -23,6 +25,7 @@ public class UniversalFragment extends Fragment {
     ImageView check1;
     ImageView check2;
     ImageView check3;
+    TextView testo;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -30,6 +33,10 @@ public class UniversalFragment extends Fragment {
         rootView = (ViewGroup) inflater.inflate(R.layout.fragment_tutorial, container, false);
 
         imageView = (ImageView)rootView.findViewById(R.id.imgTutorial);
+        check1 = (ImageView)rootView.findViewById(R.id.check1);
+        check2 = (ImageView)rootView.findViewById(R.id.check2);
+        check3 = (ImageView)rootView.findViewById(R.id.check3);
+        testo = (TextView)rootView.findViewById(R.id.testoTutorial);
         Button buttonTutorial = (Button) rootView.findViewById(R.id.buttonTutorial);
 
         buttonTutorial.setOnClickListener(new View.OnClickListener(){
@@ -40,10 +47,14 @@ public class UniversalFragment extends Fragment {
                 count++;
 
                  if (count == 1){
-                    imageView.setBackground(getResources().getDrawable(R.drawable.posate));
+                    imageView.setBackground(getDrawable(getResources(),R.drawable.posate,null));
+                     testo.setText(R.string.testo_tutorial2);
+                     check2.setBackground(getDrawable(getResources(),R.drawable.check,null));
                 }
                 else if (count == 2){
-                    imageView.setBackground(getResources().getDrawable(R.drawable.cart));
+                     imageView.setBackground(getDrawable(getResources(),R.drawable.cart,null));
+                     testo.setText(R.string.testo_tutorial3);
+                     check3.setBackground(getDrawable(getResources(),R.drawable.check,null));
                 }
 
 
@@ -53,7 +64,9 @@ public class UniversalFragment extends Fragment {
 
         if (count == 0){
             //  imageView.setBackgroundColor(getResources().getColor(R.color.color));
-            imageView.setBackground(getResources().getDrawable(R.drawable.world));
+            imageView.setBackground(getDrawable(getResources(),R.drawable.world,null));
+            testo.setText(R.string.testo_tutorial1);
+            check1.setBackground(getDrawable(getResources(),R.drawable.check,null));
         }
 
         return rootView;
